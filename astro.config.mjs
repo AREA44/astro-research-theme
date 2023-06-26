8c7733096182ed5d7772d2eff95199dd7d4a5ca2
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-06-27 00:14:31 +0700

    get localhost or ssg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://area44.github.io",
-  base: "/astro-research-theme",
+  site: process.env.CI ? "https://area44.github.io" : `http://localhost:3000`,
+  base: process.env.CI ? "/astro-research-theme" : undefined,
 });
