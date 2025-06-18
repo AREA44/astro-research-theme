a17c4166c9b311a240cd8d5341a3403f499186b7
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-06-18 16:13:07 +0700

    chore: add biome (#240)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://area44.github.io',
-  base: process.env.CI ? '/astro-research-theme/' : undefined,
-})
+  site: "https://area44.github.io",
+  base: process.env.CI ? "/astro-research-theme/" : undefined,
+});
